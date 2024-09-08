c16fe5610e5dcfe5d7f0c7a137fb2a850241c72f
ngdtan <ngdtan852003@gmail.com>
2024-09-08 18:10:10 +0700

    Lap trinh giao dien trang dang nhap

--- ecommerce/ecommerceweb/src/App.js
+++ ecommerce/ecommerceweb/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import './App.css';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import { ContextProvider } from './configs/Contexts';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
@@ -19,6 +20,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/register/" element={<Register />} />
+            <Route path="/login/" element={<Login />} />
           </Routes>
         </Router>
 
---
+++ ecommerce/ecommerceweb/src/pages/Login.js
@@ -0,0 +1,139 @@
+import React, { useState, useCallback, useContext } from 'react';
+import { useHistory, useNavigate } from 'react-router-dom';
+import { Box, Button, CircularProgress, Container, Typography, Link, Alert } from '@mui/material';
+import TextInput from '../components/TextInput';
+import axios from 'axios';
+import Contexts from '../configs/Contexts';
+//import Contexts from '../../configs/Contexts';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    //const history = useHistory();
+    //const contextValue = useContext(Contexts);
+    //const [user, dispatch] = contextValue || [{}, () => { }];
+    const [success, setSuccess] = useState('');
+    const { state, dispatch } = useContext(Contexts);
+
+    const navigate = useNavigate();
+
+    const onRefresh = useCallback(() => {
+        setUsername('');
+        setPassword('');
+        setError('');
+        setSuccess('');
+    }, []);
+
+    const login = async () => {
+        if (username === '' || password === '') {
+            setError('Tên đăng nhập và mật khẩu không được để trống.');
+            return;
+        }
+        setLoading(true);
+        try {
+            let res = await axios.post(`/o/token/`, {
+                username: username,
+                password: password,
+                client_id: 'd4fuXBp7ENwx9fmtsmBWY4XtAec9bRYlBCpAGcUm',
+                client_secret: 'swdOLBjyqXcXnQCheHqIxvoNR2ENtUgSWdMetrO9l5JBW182FaqCQKAl6K5wQwunFmTsaQfQF2dpEwC39gKxONM85GqxbT19vkaTug369JfU1E5lkYiYr4kI58gmoQ8r',
+                grant_type: 'password',
+            });
+
+            localStorage.setItem('access-token', res.data.access_token);
+            setTimeout(async () => {
+                let userRes = await axios.get(`/users/current-user/`, {
+                    headers: { Authorization: `Bearer ${res.data.access_token}` },
+                });
+
+                //const userData = user.data || [];
+                const userData = userRes.data;
+
+                dispatch({
+                    type: 'login',
+                    //payload: Array.isArray(userData) ? userData : [],
+                    payload: userData,
+                });
+
+                setSuccess('Đăng nhập thành công!');
+                setTimeout(() => {
+                    navigate('/');
+                }, 2000); // Đợi 2 giây rồi chuyển hướng
+
+
+            }, 100);
+            // setSuccess('Đăng nhập thành công!');
+            // setTimeout(() => {
+            //     navigate('/');
+            // }, 2000); // Đợi 2 giây rồi chuyển hướng
+        } catch (ex) {
+            console.log(ex);
+            setError('Tên đăng nhập hoặc mật khẩu không đúng.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <Container maxWidth="sm">
+            <Box mt={8} textAlign="center">
+                <Typography variant="h4" color="#041E42" fontWeight="bold">
+                    ECOMMERCE APP
+                </Typography>
+                <Typography variant="h5" color="#041E42" fontWeight="bold" mt={2}>
+                    ĐĂNG NHẬP
+                </Typography>
+            </Box>
+
+            <Box mt={4}>
+                <TextInput
+                    label="Tên đăng nhập..."
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                // icon={<i className="material-icons">account_circle</i>}
+                />
+                <TextInput
+                    label="Mật khẩu..."
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                // icon={<i className="material-icons">visibility</i>}
+                />
+                {error && (
+                    <Typography color="error" mt={2}>
+                        {error}
+                    </Typography>
+                )}
+
+                {success && (
+                    <Alert severity="success" mt={2}>
+                        {success}
+                    </Alert>
+                )}
+
+                <Typography color="primary" fontWeight="500" mt={2}>
+                    Quên mật khẩu?
+                </Typography>
+            </Box>
+
+            <Box mt={4} textAlign="center">
+                {loading ? (
+                    <CircularProgress />
+                ) : (
+                    <Button variant="contained" color="primary" onClick={login}>
+                        Đăng nhập
+                    </Button>
+                )}
+            </Box>
+
+            <Box mt={2} textAlign="center">
+                <Link href="/register/" variant="body2">
+                    Chưa có tài khoản? Đăng ký tại đây
+                </Link>
+            </Box>
+        </Container>
+    );
+};
+
+export default Login;
